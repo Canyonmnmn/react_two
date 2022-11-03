2509c642e4c9787cc4746eb3133b7d1370a31663
canyon <123456@qq.com>
2022-11-03 20:56:57 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-/*
- * @Description: 
- * @Author: jianguo
- * @Date: 2022-10-13 20:37:07
- * @LastEditors: jianguo
- * @LastEditTime: 2022-10-13 20:54:41
- */
-import ReactDOM from 'react-dom'
+import ReactDOM from "react-dom/client"
 import React from 'react'
-import App from './App'
+import App from './components/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
